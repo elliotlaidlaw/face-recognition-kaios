33df7362d61a8c990e9126bc952f6fd522d2cd63
FraserBarton <39245711+SaltyGiraffe@users.noreply.github.com>
2021-08-24 14:32:57 +0100

    Benchmark Script
    Separate script for benchmarking the performance of the facial recognition algorithm on the phones

--- src/index.js
+++ src/index.js
@@ -2,32 +2,53 @@
 import 'kaios-gaia-l10n';
 import './index.css';
 
+console.time("LOADING")
+
+const expressionArray = ['neutral', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised'];
+
 const tf = require('@tensorflow/tfjs')
 tf.setBackend('cpu')
 const faceapi = require('@vladmandic/face-api/dist/face-api.node-cpu.js')
+const imageEnrol = document.getElementById('imageUpload');
+
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
   faceapi.nets.faceLandmark68TinyNet.loadFromUri('./models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./models'),
-  faceapi.nets.tinyFaceDetector.loadFromUri('./models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ]).then(start);
 
 function start() {
-  var myStorage = window.localStorage
-  if (myStorage.getItem('descriptor') == null) {
-    enrol()
-  } else {
-    authenticate()
+
+  for (let i = 1; i <= 3; i++) {
+    var xmlhttp = new XMLHttpRequest();
+    var result;
+    xmlhttp.open("GET", `./descriptors/${i}.json`, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    var descriptions = []
+    var descriptorArray = JSON.parse(result)
+    for (let x in descriptorArray) {
+      descriptions.push(descriptorArray[x])
+    }
+    var floatArray = Float32Array.from(descriptions)
+    descriptions = []
+    descriptions.push(floatArray)
   }
-}
+  const labeledFaceDescriptors = new faceapi.LabeledFaceDescriptors('john', descriptions)
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.4)
 
-function enrol() {
-
-  const imageEnrol = document.getElementById('imageUpload');
+  var rand = Math.floor(Math.random() * 7)
+  console.log("Please upload an image with the following emotion: " + expressionArray[rand])
+  console.timeEnd("LOADING")
   console.log('Ready')
 
   imageEnrol.addEventListener('change', () => {
+
+    console.time("MATCHING")
 
     var image = faceapi.bufferToImage(imageEnrol.files[0])
     
@@ -35,54 +56,8 @@ function enrol() {
 
       console.log('Image loaded')
 
-      var detection = faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(true).withFaceDescriptor()
-
-      detection.then((dtn) => {
-
-        console.log("Detection loaded")
-        console.log(dtn.descriptor)
-
-        var data = JSON.stringify(dtn.descriptor)
-        console.log('saving...')
-        var myStorage = window.localStorage
-        myStorage.setItem('descriptor', data)
-        window.location.reload()
-
-      });
-    });
-  })
-}
-
-function authenticate() {
-
-  const imageUpload = document.getElementById('imageUpload');
-  const expressionArray = ['neutral', 'happy', 'sad', 'angry', 'surprised'];
-
-  console.log('loading...')
-  var myStorage = window.localStorage
-  var result = myStorage.getItem('descriptor')
-  var descriptions = []
-  var descriptorArray = JSON.parse(result)
-  for (let x in descriptorArray) {
-    descriptions.push(descriptorArray[x])
-  }
-  var floatArray = Float32Array.from(descriptions)
-  descriptions = []
-  descriptions.push(floatArray)
-
-  var labeledFaceDescriptors =  new faceapi.LabeledFaceDescriptors('john', descriptions)
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.4)
-
-  console.log('Loaded')
-  var rand = Math.floor(Math.random() * 5)
-  console.log("Please upload an image with the following emotion: "+ expressionArray[rand])
-
-  imageUpload.addEventListener('change', () => {
-    var image = faceapi.bufferToImage(imageUpload.files[0])
-    
-    image.then((img) => {
-
-      const detection = faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(true).withFaceExpressions().withFaceDescriptor()
+      var detection = faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks(true).withFaceExpressions().withFaceDescriptor()
 
       detection.then((dtn) => {
 
@@ -95,56 +70,21 @@ function authenticate() {
           const distance = result['distance']
           if (distance <= 0.4) 
           {
+            console.timeEnd("MATCHING")
             alert('You are John')
           } 
           else
           {
+            console.timeEnd("MATCHING")
             alert('you are not John. \ntry again.')
           }
         }
         else 
         {
+          console.timeEnd("MATCHING")
           alert("Liveness Test Failed")
         }
       });
     });
   })
-}
-
-function useCamera() {
-  var options = {}
-  var pictureOptions = {
-    pictureSize: null,
-    fileFormat: null
-  }
-  
-  var camera = navigator.mozCameras.getListOfCameras()[1]
-
-  function onPictureTaken(blob) {
-    console.log(blob)
-    var blobURL = URL.createObjectURL(blob)
-    var image = new Image()
-    image.src = blobURL
-    console.log(image)
-    document.body.style.backgroundImage = "url('" + blobURL + "')"
-  }
-
-  function onPictureNotTaken(error) {
-    console.warn(error)
-  }
-  
-  function onSuccess(cameraObj) {
-    var cameraControl = cameraObj.camera
-    pictureOptions.pictureSize = cameraControl.capabilities.pictureSizes[0]
-    pictureOptions.fileformat  = cameraControl.capabilities.fileFormats[0]
-  
-    console.log(cameraControl)
-    cameraControl.takePicture(pictureOptions).then(onPictureTaken, onPictureNotTaken)
-  }
-  
-  function onError(error) {
-    console.warn(error);
-  }
-  
-  navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
 }
